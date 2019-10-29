9cf137861059a9d451d9bb36a5b60856787deb30
Long <phamlong15297@gmail.com>
2019-10-29 15:07:35 +0700

    add  Load Facebook SDK for JavaScript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,5 +76,28 @@
             @yield('content')
         </main>
     </div>
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                xfbml            : true,
+                version          : 'v4.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));</script>
+
+    <!-- Your customer chat code -->
+    <div class="fb-customerchat"
+         attribution=setup_tool
+         page_id="108245903945218">
+    </div>
 </body>
 </html>
